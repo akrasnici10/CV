cd899ac5ca7acf9ebe38fa435d2f1c714ccfde43
akrasnici10 <126006574+akrasnici10@users.noreply.github.com>
2024-11-01 12:48:08 +0100

    Final Website V2

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ const App = () => {
       {/* Full-height black background */}
       <div className="fixed top-0 -z-10 h-full w-full bg-black"></div>
 
-      <div className="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
+      <div className="absolute top-0 z-[-2] h-screen w-screen bg-black bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
       
       <div className="container mx-auto px-8">
         <Navbar />
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,60 +1,62 @@
 // Import the HERO_CONTENT from index.js
 import { HERO_CONTENT } from "../constants/index"; // Adjust the path as necessary
-import profilePic from "../assets/adnan.png"
-import {motion} from "framer-motion"
+import profilePic from "../assets/adnan.png";
+import { motion } from "framer-motion";
 
 const container = (delay) => ({
-  hidden: {x: -100, opacity: 0},
+  hidden: { x: -100, opacity: 0 },
   visible: {
     x: 0,
     opacity: 1,
-    transition: {duration: 0.5, delay: delay,}
-
-  }
-})
+    transition: { duration: 0.5, delay: delay },
+  },
+});
 
 const Hero = () => {
   return (
-    <div className="border-b border-neutral-900 pb-4 lg:mb-35">
+    <div className="border-b border-neutral-0 pb-4 lg:mb-35">
       <div className="flex flex-wrap">
         <div className="w-full lg:w-1/2">
           <div className="flex flex-col items-center lg:items-start">
-            <motion.h1 
-            variants={container(0)}
-            initial="hidden"
-            animate="visible"
-            className="pb-1 text-3xl font-thick tracking-tight lg:mt-16 lg:text-8xl">
+            <motion.h1
+              variants={container(0)}
+              initial="hidden"
+              animate="visible"
+              className="pb-1 text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-thick tracking-tight lg:mt-16"
+            >
               Adnan Krasnici
             </motion.h1>
 
-            <motion.span 
-            variants={container(0.5)}
-            initial="hidden"
-            animate="visible"
-
-            className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-3xl tracking-tight text-transparent">
+            <motion.span
+              variants={container(0.5)}
+              initial="hidden"
+              animate="visible"
+              className="bg-gradient-to-r from-pink-300 via-slate-00 to-purple-500 bg-clip-text text-3xl sm:text-4xl md:text-5xl lg:text-6xl tracking-tight text-transparent"
+            >
               IT-Säkerhetsspecialist
             </motion.span>
 
-            <motion.p 
-            variants={container(1)}
-            initial="hidden"
-            animate="visible"
-            className="my-2 max-w-xl py-6 font-light tracking-tighter">
+            <motion.p
+              variants={container(1)}
+              initial="hidden"
+              animate="visible"
+              className="my-2 max-w-xl py-6 font-light tracking-tighter text-base sm:text-lg md:text-xl lg:text-lg"
+            >
               {HERO_CONTENT}
             </motion.p>
           </div>
         </div>
         <div className="w-full lg:w-1/2 lg:p-5">
-        <div className=" flex justify-center">
-            <motion.img 
-            initial={{x:100,opacity:0 }}
-            animate={{x:0, opacity: 1}}
-            transition={{duration: 1, delay: 1.2 }}
-            src={profilePic}
-             alt="Adnan Krasnici" 
-             />
-        </div> </div>
+          <div className="flex justify-center">
+            <motion.img
+              initial={{ x: 100, opacity: 0 }}
+              animate={{ x: 0, opacity: 1 }}
+              transition={{ duration: 1, delay: 1.2 }}
+              src={profilePic}
+              alt="Adnan Krasnici"
+            />
+          </div>
+        </div>
       </div>
     </div>
   );
