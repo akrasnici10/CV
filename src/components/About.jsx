47c180ffdbd4035c1c223849c94ec7965e7d1956
akrasnici10 <126006574+akrasnici10@users.noreply.github.com>
2024-11-01 21:02:08 +0100

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,7 +4,7 @@ import { ABOUT_TEXT } from "../constants";
 const About = () => {
   return (
     <div className="border-b border-neutral-900 pb-4">
-      <h2 className="my-20 text-center text-4xl"> {/* Increased margin */}
+      <h2 className="my-10 text-center text-3xl sm:text-4xl md:text-5xl lg:text-6xl"> {/* Responsive font sizes */}
         Om <span className="text-neutral-400">Mig</span>
       </h2>
       <div className="flex flex-wrap">
@@ -15,7 +15,7 @@ const About = () => {
         </div>
         <div className="w-full lg:w-1/2 order-1 lg:order-2">
           <div className="flex justify-center lg:justify-start">
-            <p className="my-2 max-w-xl py-2">
+            <p className="my-2 max-w-xl py-6 font-light tracking-tighter text-sm sm:text-base md:text-lg lg:text-lg -mt-12">
               {ABOUT_TEXT}
             </p>
           </div>
