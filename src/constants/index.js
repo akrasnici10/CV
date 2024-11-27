ab9c495712d7983911c1335c68948bc6e7e8129f
akrasnici10 <126006574+akrasnici10@users.noreply.github.com>
2024-11-27 11:57:33 +0100

    Update index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -2,8 +2,9 @@ import project1 from "../assets/projects/project-1.jpg";
 import project2 from "../assets/projects/project-2.jpg";
 import project3 from "../assets/projects/project-3.jpg";
 import project4 from "../assets/projects/project-4.jpg";
+import bild5 from "../assets/projects/bild5.png";
 
-export const HERO_CONTENT = `Jag är en entusiastisk cybersäkerhetsstudent som snart tar examen, med en solid grund i både programmering och IT-infrastruktur. Utöver mitt fokus på säkerhet är jag kunnig i Python och har praktisk erfarenhet av ELK-stacken, där jag har hjälpt Motala Kommun att stärka sin infrastruktur. Dessutom har jag erfarenhet av att bygga webbapplikationer med frontend-teknologier som React och Next.js samt backend-ramverk som Node.js, MySQL, PostgreSQL och MongoDB. Mitt mål är att fortsätta utveckla min expertis inom cybersäkerhet, hantera komplexa säkerhetsutmaningar och bidra till att skapa säkrare digitala miljöer. `;
+export const HERO_CONTENT = "Jag är en cybersäkerhetsstudent som snart tar examen, med en bra grund inom programmering och IT-infrastruktur. Jag har erfarenhet av att jobba med Python och ELK-stacken och har hjälpt Motala Kommun att stärka sin infrastruktur. Jag kan också bygga webbapplikationer med verktyg som React, Next.js, Node.js, MySQL, PostgreSQL och MongoDB. Mitt mål är att utvecklas inom cybersäkerhet och bidra till säkrare digitala miljöer" ;
 
 export const ABOUT_TEXT = `Hej!
 Jag heter Adnan och är 24 år från Motala. Jag pluggar IT och älskar att lösa olika typer av problem. Jag tycker att det är kul att vrida och vända på utmaningar tills man hittar den bästa lösningen. I skolprojekt och i sociala sammanhang så trivs jag bäst när jag får bolla idéer med andra. När jag inte sitter vid datorn så håller jag mig aktiv. Jag spelar lokalfotboll, går på gym och och är i allmänhet väldigt sportintresserad. Jag tror att det är viktigt att utvecklas som person, inte bara inom IT. Därför försöker jag vara en schysst lagspelare som bidrar med bra energi, oavsett om det är på planen eller i ett projektarbete.`;
@@ -13,26 +14,21 @@ export const EXPERIENCES = [
     year: "2023 - Present",
     role: "IT-Säkerhetsteknikerstudent",
     company: "TUC Yrkeshögskola.",
-    description: `Som IT-säkerhetstekniker har jag fått en bred kompetens inom både nätverk och säkerhet. Under utbildningen har jag jobbat mycket med praktiska övningar där vi satt upp och säkrat olika IT-miljöer. Jag har blivit särskilt duktig på att hantera nätverksutrustning som brandväggar, switchar och routrar, samt att konfigurera dem på ett säkert sätt.
-En stor del av min kunskap ligger i att bygga och hantera systemlösningar med Docker och Active Directory. Jag kan även utveckla säkra skript i Python för att automatisera olika processer och säkerhetskontroller. Det roligaste med utbildningen har varit att få kombinera teori med hands-on erfarenhet, där vi fått lösa verklighetstrogna problem som IT-avdelningar stöter på dagligen.
-Mina tekniska kunskaper omfattar: Nätverkssäkerhet (brandväggar, switchar, routrar) Containerhantering med Docker, Programmering i Python, Windows Server och Active Directory samt säker systemkonfiguration`,
+    description: "Som IT-säkerhetstekniker har jag byggt en bred kompetens inom nätverk och säkerhet. Under utbildningen har jag jobbat med praktiska övningar för att sätta upp och säkra olika IT-miljöer. Jag är van vid att hantera nätverksutrustning som brandväggar, switchar och routrar och att konfigurera dem säkert. Jag har också erfarenhet av att bygga och hantera system med Docker och Active Directory, samt att skriva säkra skript i Python för att automatisera processer och säkerhetskontroller. Det roligaste har varit att kombinera teori med praktiska övningar för att lösa verkliga problem inom IT-säkerhet.",
     technologies: ["Docker", "Python", "Active Directory", "MySQL"],
   },
   {
     year: "2022 - 2023",
     role: "Abbonemangsförsäljare",
     company: "Elgiganten",
-    description: `Under min tid som abonnemangsförsäljare på Elgiganten utvecklade jag starka kundrelationer och lärde mig konsten att verkligen lyssna på kundens behov. Det var mer än bara försäljning – det handlade om att förstå varje kunds unika situation och hjälpa dem hitta den bästa lösningen, vare sig det gällde mobil-, bredband- eller TV-abonnemang.
-I rollen fick jag dagligen öva på att förklara komplexa tekniska lösningar på ett enkelt och begripligt sätt. Detta har gett mig ovärderlig erfarenhet i att kommunicera tydligt och anpassa mitt sätt att förklara beroende på vem jag pratar med. Arbetet i butiksmiljön stärkte också min förmåga att samarbeta i team och hantera stressiga situationer, särskilt under högsäsong.
-Några viktiga färdigheter jag utvecklade: Stark kommunikationsförmåga, Problemlösning i kundmöten, Förmåga att förklara tekniska lösningar enkelt, Teamwork och samarbete, Stresstålighet`,
+    description: "Som abonnemangsförsäljare på Elgiganten byggde jag starka kundrelationer och lärde mig att lyssna på kundens behov. Det handlade om att förstå varje kunds situation och hitta den bästa lösningen, oavsett om det gällde mobil-, bredbands- eller TV-abonnemang. Jag fick öva på att förklara tekniska lösningar på ett enkelt sätt, vilket gav mig värdefull erfarenhet i tydlig kommunikation och anpassning efter kunden. Arbetet i butiksmiljö stärkte också min samarbetsförmåga och stresstålighet, särskilt under högsäsong.",
     technologies: ["Problemlösning", "Kommunikation", "Teamwork & Sammarbete ", ],
   },
   {
     year: "2019 - 2022",
     role: "Servicemedarbetare",
     company: "Elgiganten",
-    description: `Som servicemedarbetare på Elgiganten fick jag mitt första riktiga hands-on med IT-problemlösning. Varje dag mötte jag kunder med olika tekniska utmaningar – allt från virus-infekterade datorer och hackade konton till krånglande nätverk och trasiga enheter. Det var här mitt intresse för IT-säkerhet verkligen växte fram, när jag såg hur många som faktiskt drabbas av olika typer av IT-problem och cyberattacker.
-Jobbet gav mig värdefull erfarenhet i att felsöka tekniska problem och hitta lösningar under tidspress. Jag lärde mig snabbt att kommunicera tekniskt komplexa problem på ett sätt som kunderna förstod, samtidigt som jag fick grundläggande kunskaper i datasäkerhet och virushantering.Det var detta jobb som öppnade mina ögon för hur viktigt det är med IT-säkerhet i dagens digitala värld, vilket senare ledde mig till att utbilda mig inom området. Viktiga erfarenheter från tiden som servicemedarbetare: Praktisk erfarenhet av IT-felsökning, Grundläggande kunskaper i datasäkerhet, Problemlösning under tidspress, Teknisk kundsupport.`,
+    description: "Som servicemedarbetare på Elgiganten fick jag min första riktiga erfarenhet av IT-problemlösning. Jag mötte kunder med olika tekniska problem – från virusinfekterade datorer och hackade konton till krånglande nätverk och trasiga enheter. Det var här mitt intresse för IT-säkerhet började, när jag såg hur många som drabbas av IT-problem och cyberattacker. Jobbet lärde mig att felsöka snabbt under tidspress och att förklara tekniska problem på ett sätt som kunderna förstod. Jag fick också grundläggande kunskaper i datasäkerhet och virushantering, vilket inspirerade mig att utbilda mig inom IT-säkerhet.",
     technologies: ["Praktisk erfarenhet av IT-felsökning", "Problemlösning under tidspress",],
   },
   {
@@ -57,8 +53,8 @@ export const PROJECTS = [
     title: "Task Management App",
     image: project2,
     description:
-      "Ett verktyg för att strukturera, prioritera och följa upp dina dagliga uppgifter och projekt. Detta var ett av de första projekten jag skapade!",
-    technologies: ["HTML", "CSS", "Angular", "Firebase"],
+      "Ett verktyg för att strukturera, prioritera och följa upp dina dagliga uppgifter och projekt. Det unika med denna appen är att den är skapad med python för både front och backend samt user authentication genom användning av 'Flask' och 'SQLAlchemy' ",
+    technologies: ["Python", "Flask", "SQLAlchemy",],
   },
   {
     title: "Portfoliohemsida",
@@ -67,6 +63,13 @@ export const PROJECTS = [
       "En personlig CV-hemsida som visar mina projekt, kompetenser och kontaktinformation.",
     technologies: ["HTML", "CSS", "React",],
   },
+  {
+    title: "ELK Stack Optimering för Kommunens Infrastruktur",
+    image: bild5,
+    description:
+      "Som praktikant hos Motala kommun så arbetade jag med ELK stack. Där satte jag upp nya regler, förbättrade alerts, och filtrerade bort falska larm. Dessutom använde jag machine learning för att göra systemet ännu mer träffsäkert.",
+    technologies: ["ELK-Stack", "SIEM", "EDR",],
+  }
  
 ];
 
